de22616c58af8386db9e95a0f21be2804829f661
tommytoyoutoo <tommytoyou@gmail.com>
2021-03-07 13:39:35 -0600

    db set up working on auth

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,15 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+const Post = require('../../models/Post');
+
+module.exports = {
+    Query: {
+            async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+            }
+        }
+}
---
+++ graphql/typeDefs.js
@@ -0,0 +1,31 @@
+const gql = require('graphql-tag'); 
+
+
+// this is a tag template string
+module.exports = gql`
+   type Post{
+      id: ID!
+      body: String!
+      createdAt: String!
+      username: String!
+   }
+   type User{
+       id: String!
+       email: String!
+       token: String!
+       username: String!
+       createdAt: String!
+   }
+   input RegisterInput{
+       username: String!
+       password: String!
+       confirmPassword: String!
+       email: String!
+   }
+  type Query{
+      getPosts: [Post]
+  }
+  type mutation{
+      register(registerInput: RegisterInput): User!
+  }
+`;
